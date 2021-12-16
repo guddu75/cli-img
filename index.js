0a477cab0514642f8188bff888fda9fef8f0fe20
Debapriya Majumder <guddumajumder75@gmail.com>
2021-12-16 14:17:04 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+
+/**
+ * 
+ * @author Debapriya Majumder
+ * 
+ */
+
+const init = require('./utils/init');
+const cli = require('./utils/cli');
+const log = require('./utils/log');
+
+const input = cli.input;
+const flags = cli.flags;
+
+const { clear , debug } = flags;
+
+
+(async () => {
+
+    init({clear});
+    input.includes(`help`) && cli.showHelp(0);
+
+    debug && log(flags);
+
+})();
